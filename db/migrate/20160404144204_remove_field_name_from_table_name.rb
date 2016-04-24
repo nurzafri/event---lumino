a1e93aad64065b6a208b8dcc280a3dea010ad090
nurzafri <nurzafri@gmail.com>
2016-04-25 01:12:27 +0800

    remove migrate remove_file, modify UI

--- db/migrate/20160404144204_remove_field_name_from_table_name.rb
+++
@@ -1,7 +0,0 @@
-class RemoveFieldNameFromTableName < ActiveRecord::Migration
-  def change
-    remove_column :occasions, :date, :date
-    remove_column :occasions, :time_start, :time
-    remove_column :occasions, :time_end, :time
-  end
-end
